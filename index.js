2c539fb51d183d9a2d2028b076987af545b02b94
WiecznyX <bmunkhtushig00@gmail.com>
2022-08-27 00:51:12 +0800

    nice

---
+++ index.js
@@ -0,0 +1,26 @@
+
+// let button = document.getElementsByClassName("btn");
+
+// console.log(button);
+
+
+// function myfunction(style) {
+//     document.querySelector("btn").style.display = "flex";
+// }
+
+function nice() {
+
+    if(document.getElementById("photo").style.display == "none"){
+        document.getElementById("photo").style.display = "flex";
+    } else {
+        document.getElementById("photo").style.display = "none";
+    }
+}
+function niceTwo() {
+
+    if(document.getElementById("photoTwo").style.display == "none"){
+        document.getElementById("photoTwo").style.display = "flex";
+    } else {
+        document.getElementById("photoTwo").style.display = "none";
+    }
+}
